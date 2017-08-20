49ce7f94c8209b7686de685a470627dd31c09f98
prgfan <prgfan@hotmail.com>
2017-08-20 15:00:12 -0400

    ئابونت ئۆز ئورنىنى بەلگىلەش ئىقتىدارى قوشۇلدى

--- DriverClient/src/com/tawush/checkmark/activity/TaskActivity.java
+++ DriverClient/src/com/tawush/checkmark/activity/TaskActivity.java
@@ -2,23 +2,32 @@ package com.tawush.checkmark.activity;
 
 import android.support.v7.app.ActionBarActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.zxing.client.android.CaptureActivity;
 import com.tawush.checkmark.R;
 import com.tawush.checkmark.R.id;
 import com.tawush.checkmark.R.layout;
 import com.tawush.checkmark.R.menu;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-public class TaskActivity extends ActionBarActivity implements OnMapReadyCallback{
+public class TaskActivity extends ActionBarActivity implements OnMapReadyCallback,LocationListener{
 
+	private GoogleMap mMap;
+	LocationManager locManager;
 	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +37,24 @@ public class TaskActivity extends ActionBarActivity implements OnMapReadyCallbac
 		MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        
+        this.locManager=(LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 	}
 
+	
+	@Override
+	public void onResume()
+	{
+		locManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 200, 10, this);
+		super.onResume();
+	}
+	@Override
+	public void onPause()
+	{
+		locManager.removeUpdates(this);
+		super.onPause();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -52,7 +77,7 @@ public class TaskActivity extends ActionBarActivity implements OnMapReadyCallbac
 	@Override
 	public void onMapReady(GoogleMap arg0) {
 		// TODO Auto-generated method stub
-		
+		this.mMap=arg0;
 	}
 	
 	public void onScan(View view)
@@ -60,4 +85,41 @@ public class TaskActivity extends ActionBarActivity implements OnMapReadyCallbac
 		Intent intent=new Intent(this,CaptureActivity.class);
 		startActivity(intent);
 	}
+
+	
+	/////////Location Manager ////////////
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		if(location==null)
+		{
+			Toast.makeText(this, "location is null", Toast.LENGTH_SHORT).show();
+		}else {
+			if(mMap!=null)
+			{
+				LatLng latlng=new LatLng(location.getLatitude(),location.getLongitude());
+				mMap.moveCamera(CameraUpdateFactory.newLatLng(latlng));
+				mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+			}
+		}
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+	////////////Location Manager end ////////////////
 }
